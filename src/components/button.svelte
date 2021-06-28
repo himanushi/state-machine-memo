e48381fdd9c0d6c402ee446950d2edd37e8fed29
himanushi <himanushi@users.noreply.github.com>
2021-06-28 13:20:36 +0900

    add component

---
+++ src/components/button.svelte
@@ -0,0 +1,7 @@
+<button on:click><slot /></button>
+
+<style lang="scss">
+button {
+  @apply p-3 rounded bg-blue-500 text-blue-50;
+}
+</style>
---
+++ src/components/icon-button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+let className = "";
+export { className as class };
+export let disabled = false;
+</script>
+
+<button {disabled} class={className} on:click>
+  <slot />
+</button>
+
+<style>
+button {
+  /* 子要素の位置 */
+  @apply flex items-center justify-center rounded-full;
+  /* color */
+  @apply bg-white bg-opacity-0;
+  /* animation */
+  @apply transition duration-200;
+  /* hover */
+  @apply hover:bg-opacity-20;
+  /* click */
+  @apply active:bg-opacity-50 focus:outline-none;
+  /* disabled */
+  @apply disabled:opacity-50;
+}
+</style>
