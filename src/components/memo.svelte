a2df7459e1395fd73a28bf83fc7d8e5a0a535c08
himanushi <himanushi@users.noreply.github.com>
2021-06-28 13:40:52 +0900

    memo 共通化

---
+++ src/components/memo.svelte
@@ -0,0 +1,29 @@
+<script>
+import Code from "~/components/code.svelte";
+import Inspect from "~/components/inspect.svelte";
+
+export let code: string;
+
+let ready = false;
+const onReady = () => (ready = true);
+</script>
+
+<div class="memo">
+  {#if ready}
+    <div class="content">
+      <slot />
+    </div>
+  {/if}
+  <div class="inspect">
+    <Inspect class="w-[800px] h-[500px]" on:ready={onReady} />
+  </div>
+  <div class="code">
+    <Code {code} />
+  </div>
+</div>
+
+<style lang="scss">
+.memo {
+  @apply flex flex-col items-center space-y-6 py-10;
+}
+</style>
