528b3652cb7f851cacc21b25f1d348f02905d160
himanushi <himanushi@users.noreply.github.com>
2021-06-28 13:20:52 +0900

    top page 更新

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,5 +1,42 @@
 <script lang="ts">
 import { goto } from "@roxi/routify";
+import Button from "~/components/button.svelte";
+import IconButton from "~/components/icon-button.svelte";
+import Github from "~/icons/github.svelte";
 </script>
 
-<button on:click={() => $goto("/memo/traffic-light")}>信号機</button>
+<main>
+  <span> State Machine MEMO </span>
+  <li>
+    <ul>
+      <Button on:click={() => $goto("/memo/traffic-light")}>
+        Traffic Light
+      </Button>
+    </ul>
+    <ul>
+      <a href="https://github.com/himanushi/xstate-memo" target="_blank">
+        <IconButton class="w-10 h-10">
+          <Github class="w-10 h-10" />
+        </IconButton>
+      </a>
+    </ul>
+  </li>
+</main>
+
+<style lang="scss">
+main {
+  @apply h-full flex flex-col items-center justify-center;
+
+  span {
+    @apply text-4xl mb-6;
+  }
+
+  li {
+    @apply flex flex-col space-y-3;
+
+    ul {
+      @apply self-center;
+    }
+  }
+}
+</style>
